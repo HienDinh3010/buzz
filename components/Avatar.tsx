9a07ca155d70104a4652205dd4a399550200a7a5
HienDinh <dnmhien3010@gmail.com>
2024-07-07 06:59:39 -0500

    Moving users folder under api folder of pages

---
+++ components/Avatar.tsx
@@ -0,0 +1,28 @@
+import useUser from "@/hooks/useUser";
+import { useCallback } from "react";
+
+interface AvatarProps {
+    userId: string;
+    isLarge?: boolean;
+    hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+    userId,
+    isLarge,
+    hasBorder
+}) => {
+    const { data: fetchedUser } = useUser(userId);
+    const onClick = useCallback(
+      (event: any) => {
+        event.stopPropagation();
+      },
+      [event],
+    )
+    
+    return (
+        <div></div>
+    )
+}
+
+export default Avatar;
